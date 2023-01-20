9287661f864de142d735aa4fe64829a5dccdec88
Dmitry Bober <bober113d@gmail.com>
2023-01-20 15:59:56 +0500

    prog3

---
+++ prog3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int[] randomArray (int length)
+{
+    int [] array = new int [length];
+    for (int i=0; i < array.Length; i++){
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+
+void showArray (int[] array)
+{
+    for(int i=0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.Write(" -> ");
+}
+
+int [] array = randomArray(8);
+showArray(array);
+Console.WriteLine($"{array.Max() - array.Min()}");
